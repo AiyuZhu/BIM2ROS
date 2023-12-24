967234f540eb594eee6642bd60fafa589f511b92
aiyu <a.zhu@tue.nl>
2023-12-24 17:03:11 +0800

    feat: config test_model_in_ros

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/root/BIM2ROS/build/gtest/cmake_install.cmake")
+  include("/root/BIM2ROS/build/unit_without_beam_in_b_room/cmake_install.cmake")
 
 endif()
 
